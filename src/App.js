cf945c85034efac035fe7d6df4cfbc796cf00a80
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-06-03 18:44:45 +0400

    completed mockup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
+import dummyData from './dummy-data';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = dummyData;
+	}
+	render() {
+		return (
+			<div className="App">
+				<SearchBar />
+				{this.state.map(post => <PostContainer key={post.id} post={post} />)}
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/PostContainer/PostContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const postContainer = props => {
+	return (
+		<div>
+			<div>Post Header</div>
+			<div>Post Image</div>
+			<div>CommentSection</div>
+		</div>
+	);
+};
+
+export default postContainer;
